9866deb2ac67e4ccae379db1dc8474e29cf1995e
altarulas <altarulas09@gmail.com>
2023-03-14 18:18:51 +0300

    chore: fix design bug

--- src/pages/Doctor/Pages/Home.jsx
+++ src/pages/Doctor/Pages/Home.jsx
@@ -1,12 +1,88 @@
+import { AppointmentWrapper, ContentWrapper, HomeBase, InfoWrapper, PatientName, Title } from "../Styles/Home.style";
+
+import { Button } from "@mui/material";
 import DefaultBox from "..//..//..//Components/DefaultBox/DefaultBox"
-import { HomeBase } from "../Styles/Home.style";
 
 const Home = () => {
 
     return (
         <HomeBase>
             <DefaultBox width={"95%"} height={"90%"} background={"white"} >
-                <h1>Doctor Page</h1>
+                <Title>My Appointments</Title>
+                <AppointmentWrapper>
+                    <ContentWrapper>
+                        <InfoWrapper>
+                            <PatientName>Gustavo Fringe</PatientName>
+                        </InfoWrapper>
+                        <Button className="home-detail" color="primary" variant="contained">
+                            DETAIL
+                        </Button>
+                        <Button className="send-result" color="success" variant="contained">
+                            SEND TEST RESULT
+                        </Button>
+                    </ContentWrapper>
+
+                    <ContentWrapper>
+                        <InfoWrapper>
+                            <PatientName>Gustavo</PatientName>
+                        </InfoWrapper>
+                        <Button className="home-detail" color="primary" variant="contained">
+                            DETAIL
+                        </Button>
+                        <Button className="send-result" color="success" variant="contained">
+                            SEND TEST RESULT
+                        </Button>
+                    </ContentWrapper>
+
+                    <ContentWrapper>
+                        <InfoWrapper>
+                            <PatientName>Gustavo</PatientName>
+                        </InfoWrapper>
+                        <Button className="home-detail" color="primary" variant="contained">
+                            DETAIL
+                        </Button>
+                        <Button className="send-result" color="success" variant="contained">
+                            SEND TEST RESULT
+                        </Button>
+                    </ContentWrapper>
+
+                    <ContentWrapper>
+                        <InfoWrapper>
+                            <PatientName>Gustavo</PatientName>
+                        </InfoWrapper>
+                        <Button className="home-detail" color="primary" variant="contained">
+                            DETAIL
+                        </Button>
+                        <Button className="send-result" color="success" variant="contained">
+                            SEND TEST RESULT
+                        </Button>
+                    </ContentWrapper>
+
+                    <ContentWrapper>
+                        <InfoWrapper>
+                            <PatientName>Gustavo</PatientName>
+                        </InfoWrapper>
+                        <Button className="home-detail" color="primary" variant="contained">
+                            DETAIL
+                        </Button>
+                        <Button className="send-result" color="success" variant="contained">
+                            SEND TEST RESULT
+                        </Button>
+                    </ContentWrapper>
+
+                    <ContentWrapper>
+                        <InfoWrapper>
+                            <PatientName>Gustavo</PatientName>
+                        </InfoWrapper>
+                        <Button className="home-detail" color="primary" variant="contained">
+                            DETAIL
+                        </Button>
+                        <Button className="send-result" color="success" variant="contained">
+                            SEND TEST RESULT
+                        </Button>
+                    </ContentWrapper>
+                </AppointmentWrapper>
+
             </DefaultBox>
         </HomeBase >
     );
--- src/pages/Doctor/Styles/Home.style.jsx
+++ src/pages/Doctor/Styles/Home.style.jsx
@@ -8,10 +8,69 @@ export const HomeBase = styled.div`
     justify-content: center;
 `;
 
-export const Box = styled.div`
-    width: ${props => props.width};
-    height: ${props => props.height};
-    background-color: white;
-    border-radius: 10px;
-    margin: 20px 12px;
+export const Title = styled.div`
+    width: 60%;
+    height: 20%;
+    display: flex;
+    align-items: center;
+    margin-left: 24px;
+    font-size: 28px;
+
+    @media screen and (max-width: 585px) {
+        font-size: 20px;
+    }
 `;
+
+export const AppointmentWrapper = styled.div`
+    width: 100%;
+    height: 70%;
+    display: flex;
+    flex-direction: column;
+    padding: 24px;
+    overflow-y: auto;
+    
+    @media screen and (max-width: 585px) {
+        
+    }
+`;
+
+export const ContentWrapper = styled.div`
+    width: 80%;
+    height: 90px;
+    display: flex;
+    align-items: center;
+    background-color: #737baf;
+    border-radius: 10px;
+    padding: 0px 12px;
+    margin: 12px 0px;
+
+    @media screen and (max-width: 656px) {
+        width: 100%;
+    }
+`;
+
+export const InfoWrapper = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width: 585px) {
+        flex-direction: column;
+        align-items: center;
+        margin-right: 20px;
+    }
+`;
+
+export const PatientName = styled.div`
+    width: 100%;
+    margin: 0px 12px;
+    color: white;
+
+    @media screen and (max-width: 585px) {
+        font-size: 13px;
+    }
+`;
+
+
+
