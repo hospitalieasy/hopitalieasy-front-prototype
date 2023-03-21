b8615834003182a6386dd9b006d107b8d3af327f
altarulas <altarulas09@gmail.com>
2023-03-21 21:59:58 +0300

    commit: add google maps

--- src/pages/patient/Pages/Home.jsx
+++ src/pages/patient/Pages/Home.jsx
@@ -1,39 +1,120 @@
-import { HomeBase, Section } from "../Styles/Home.style";
+import { ContentWrapper, HomeBase, MapWrapper, SectionBanner, SectionMap, Title } from "../Styles/Home.style";
+import { useEffect, useRef, useState } from "react";
 
 import DefaultBox from "..//..//..//Components/DefaultBox/DefaultBox";
-import DoctorReview from "../../../Utilities/Components/DoctorReview/DoctorReview";
-import SlideShowAppScreen from "../../../Components/SlideShow/SlideShowAppScreen"
+import SlideShowAppScreen from "../../../Components/SlideShow/SlideShowAppScreen";
 
 const Home = () => {
+    const [userLocation, setUserLocation] = useState(null);
+    const [hospitals, setHospitals] = useState([]);
+    const [pharmacies, setPharmacies] = useState([]);
+    const hospitalMapRef = useRef(null); // separate mapRef for hospitals
+    const pharmacyMapRef = useRef(null); // separate mapRef for pharmacies
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const userLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                setUserLocation(userLocation);
+
+                // Search for nearby hospitals
+                const hospitalRequest = {
+                    location: userLocation,
+                    radius: "5000",
+                    type: ["hospital"],
+                };
+                const hospitalService = new window.google.maps.places.PlacesService(
+                    hospitalMapRef.current // use separate mapRef for hospitals
+                );
+                hospitalService.nearbySearch(hospitalRequest, (results, status) => {
+                    if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                        setHospitals(results);
+                    }
+                });
+
+                // Search for nearby pharmacies
+                const pharmacyRequest = {
+                    location: userLocation,
+                    radius: "5000",
+                    type: ["pharmacy"],
+                };
+                const pharmacyService = new window.google.maps.places.PlacesService(
+                    pharmacyMapRef.current
+                );
+                pharmacyService.nearbySearch(pharmacyRequest, (results, status) => {
+                    if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                        setPharmacies(results);
+                    }
+                });
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        if (userLocation) {
+            const { lat, lng } = userLocation;
+
+            // Create separate map objects for hospitals and pharmacies
+            const hospitalMap = new window.google.maps.Map(hospitalMapRef.current, {
+                zoom: 14,
+                center: { lat, lng },
+            });
+
+            const pharmacyMap = new window.google.maps.Map(pharmacyMapRef.current, {
+                zoom: 14,
+                center: { lat, lng },
+            });
+
+            new window.google.maps.Marker({
+                position: { lat, lng },
+                map: hospitalMap, // add marker to hospital map object
+            });
+
+            hospitals.forEach((hospital) => {
+                new window.google.maps.Marker({
+                    position: hospital.geometry.location,
+                    map: hospitalMap, // add marker to hospital map object
+                });
+            });
+
+            pharmacies.forEach((pharmacy) => {
+                new window.google.maps.Marker({
+                    position: pharmacy.geometry.location,
+                    map: pharmacyMap, // add marker to pharmacy map object
+                });
+            });
+        }
+    }, [userLocation, hospitals, pharmacies]);
 
     return (
         <HomeBase>
-            <Section width={"95%"} height={"100%"}>
-
+            <SectionBanner width={"95%"} height={"50%"}>
                 <DefaultBox
                     width={"98%"}
-                    height={"55%"}
+                    height={"90%"}
                     background={"white"}
                     margin={"20px 12px"}
                 >
                     <SlideShowAppScreen />
                 </DefaultBox>
+            </SectionBanner>
 
-                {/* <DefaultBox width={"100%"} height={"45%"} background={"white"} margin={"20px 12px"}>
-                    <DoctorReview title={"Quick Appointments (Future Content)"} />
-                </DefaultBox> */}
+            <SectionMap width={"95%"} height={"50%"}>
+                <ContentWrapper>
+                    <Title>Closest Hospitals</Title>
+                    <MapWrapper ref={hospitalMapRef} />
+                </ContentWrapper>
 
-                <DefaultBox width={"100%"} height={"220px"} background={"white"} margin={"20px"}>
-                    <h1>Map 1</h1>
-                </DefaultBox>
-
-                <DefaultBox width={"100%"} height={"220px"} background={"white"} margin={"20px"}>
-                    <h1>Map 2</h1>
-                </DefaultBox>
-
-            </Section>
+                <ContentWrapper>
+                    <Title>Closest Pharmacies</Title>
+                    <MapWrapper ref={pharmacyMapRef} />
+                </ContentWrapper>
+            </SectionMap>
         </HomeBase >
     );
-}
+};
 
 export default Home;
--- src/pages/patient/styles/Home.style.jsx
+++ src/pages/patient/styles/Home.style.jsx
@@ -6,13 +6,50 @@ export const HomeBase = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    `;
+    flex-direction: column;
+`;
 
-export const Section = styled.div`
+export const SectionBanner = styled.div`
     height: ${props => props.height};
     width: ${props => props.width};
     display: flex;
     align-items: center;
     flex-wrap: wrap;
+`;
+
+export const SectionMap = styled.div`
+    height: ${props => props.height};
+    width: ${props => props.width};
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
+
+export const ContentWrapper = styled.div`
+    height: 90%;
+    width: 45%;
+    margin: 0px 20px;
+
+    @media screen and (max-width: 904px) {
+        width: 100%;
+    }
+`;
+
+export const Title = styled.div`
+    width: 100%;
+    color: white;
+    font-size: 24px;
+    margin: 24px 0px;
+    
+    @media screen and (max-width: 585px) {
+       font-size: 19px;
+    }
+`;
+
+export const MapWrapper = styled.div`
+    height: 65%;
+    width: 100%;
+    border-radius: 10px;
 `;
 
