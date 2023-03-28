686fe7ebded5553500a7eed380753aa747f8b931
altarulas <altarulas09@gmail.com>
2023-03-28 03:14:09 +0300

    commit: enter content of home page slider

--- src/Components/SlideShow/SlideShowAppScreen.jsx
+++ src/Components/SlideShow/SlideShowAppScreen.jsx
@@ -1,7 +1,11 @@
-import { Card, ContentWrapper, SlideShowBase, Text, TextWrapper, Title } from './SlideShowAppScreen.style';
+import { Card, ContentWrapper, Image, Logo, SlideShowBase, Text, TextWrapper, Title } from './SlideShowAppScreen.style';
 
 import React from 'react';
 import ReactSwipe from 'react-swipe';
+import appointment from "..//..//Utilities/Images/appointment.png"
+import map from "..//..//Utilities/Images/map.png"
+import register from "..//..//Utilities/Images/register.png"
+import result from "..//..//Utilities/Images/result.png"
 
 const SlideShowAppScreen = () => {
 
@@ -23,43 +27,57 @@ const SlideShowAppScreen = () => {
             >
                 <Card>
                     <ContentWrapper>
-
                         <Title>Register free</Title>
-                        <Text>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aliquid placeat hic illo distinctio, dolore pariatur molestiae quo cum. Nam corrupti sunt nostrum exercitationem eveniet dignissimos ex earum, tempore at.
-                        </Text>
-
+                        <TextWrapper>
+                            <Text>
+                                You can register to Hospitaleasy for free and then start using the new generation online hospital system directly!
+                            </Text>
+                            <Logo>
+                                <Image src={register} />
+                            </Logo>
+                        </TextWrapper>
                     </ContentWrapper>
                 </Card>
 
                 <Card>
                     <ContentWrapper>
                         <Title>Get Appointment</Title>
-                        <Text>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aliquid placeat hic illo distinctio, dolore pariatur molestiae quo cum. Nam corrupti sunt nostrum exercitationem eveniet dignissimos ex earum, tempore at.
-                        </Text>
+                        <TextWrapper>
+                            <Text>
+                                You can choose the doctors, days and hours that are suitable for you and get the appointment you want.
+                            </Text>
+                            <Logo>
+                                <Image src={appointment} />
+                            </Logo>
+                        </TextWrapper>
                     </ContentWrapper>
                 </Card>
 
                 <Card>
                     <ContentWrapper>
-
                         <Title>See your test result</Title>
-                        <Text>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aliquid placeat hic illo distinctio, dolore pariatur molestiae quo cum. Nam corrupti sunt nostrum exercitationem eveniet dignissimos ex earum, tempore at.
-                        </Text>
-
+                        <TextWrapper>
+                            <Text>
+                                View the test results prepared by your doctor immediately after making the appointment that suits you!
+                            </Text>
+                            <Logo>
+                                <Image src={result} />
+                            </Logo>
+                        </TextWrapper>
                     </ContentWrapper>
                 </Card>
 
                 <Card>
                     <ContentWrapper>
-
-                        <Title>See the nearest hospitals && pharmacy</Title>
-                        <Text>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis aliquid placeat hic illo distinctio, dolore pariatur molestiae quo cum. Nam corrupti sunt nostrum exercitationem eveniet dignissimos ex earum, tempore at.
-                        </Text>
-
+                        <Title>See the nearest hospitals && pharmacies</Title>
+                        <TextWrapper>
+                            <Text>
+                                You can browse the map not only online, but also the nearest hospitals and pharmacies, which will be listed according to your location.
+                            </Text>
+                            <Logo>
+                                <Image src={map} />
+                            </Logo>
+                        </TextWrapper>
                     </ContentWrapper>
                 </Card>
             </ReactSwipe>
--- src/Components/SlideShow/SlideShowAppScreen.style.jsx
+++ src/Components/SlideShow/SlideShowAppScreen.style.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SlideShowBase = styled.div`
     height: 100%;
     width: 100%;
-    padding: 40px;
+    padding: 28px;
 
     @media screen and (max-width: 585px) {
         padding: 24px;
@@ -18,29 +18,60 @@ export const Card = styled.div`
 export const ContentWrapper = styled.div`
     width: 100%;
     height: 100%;
-    padding: 24px;
+    display: flex;
+    padding: 0px 24px;
+    align-items: center;
+    flex-direction: column;
 
     @media screen and (max-width: 585px) {
+    }
+`;
+
+export const TextWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    margin-right: 12px;
+    @media screen and (max-width: 585px) {
+    }
+`;
+
+export const Logo = styled.div`
+    height: 100%;
+    margin-left: 12px;
+`;
+
+export const Image = styled.img`
+    height: 150px;
+    width: 150px;
+
+    @media screen and (max-width: 585px) {
+        height: 100px;
+        width: 100px;
     }
 `;
 
 export const Title = styled.div`
     width: 100%;
     height: 100%;
-    margin-bottom: 40px;
     font-size: 26px;
     color: black;
     letter-spacing: 1px;
     font-family: 'Roboto Condensed', sans-serif;
+    margin-bottom: 4px;
 
     @media screen and (max-width: 585px) {
         font-size: 20px;
+        margin-bottom: 24px;
     }
 `;
 
 
 export const Text = styled.div`
     width: 100%;
+    max-width: 750px;
     height: 100%;
     font-size: 18px;
     color: black;
